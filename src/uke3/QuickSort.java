30b286d9b33dc0cc5ee4b057e527e54c6754a15c
NimaAB <nimabewrani98@gmail.com>
2020-09-14 09:52:43 +0200

    quicksort middel element

--- src/uke3/QuickSort.java
+++ src/uke3/QuickSort.java
@@ -4,13 +4,9 @@ import java.util.Arrays;
 
 public class QuickSort {
 
-    static void swap(int a,int b){
-        int temp = a;
-        a = b;
-        b = temp;
-    }
-    //Quick Sort implementation:
-    static int partisjon(int [] a,int begin,int end) {
+    //Takes the last element as pivot:
+    static int partisjon1(int [] a,int begin,int end) {
+        int pivot = end;
         int pi = begin;
         for (int i = begin; i < end; i++){
             if (a[i] < a[end]) {
@@ -20,17 +16,49 @@ public class QuickSort {
                 pi++;
             }
         }
-        int temp = a[end];
-        a[end] = a[pi];
+        int temp = a[pivot];
+        a[pivot] = a[pi];
         a[pi] = temp;
         return pi;
     }
     static void quickSort(int [] a,int begin,int end){
         if(end <= begin) return;
-
-        int pP = partisjon(a, begin, end);
+        int pP = partisjon1(a, begin, end);
         quickSort(a, begin, pP-1);
         quickSort(a,pP+1, end);
+    }
+    //Takes the middle element of the array as a pivot:
+    static int partition2(int[] a,int begin,int end){
+        int pivot = begin + (end - begin)/2;
+        int temp = end;
+        end = pivot;
+        pivot = temp;
+        int left = begin;
+
+        for(int i =begin; i<end;++i){
+            if(a[i]<a[pivot]){
+                int temp1 = a[left];
+                a[left]=a[i];
+                a[i] = temp1;
+                left++;
+            }
+        }
+        int temp2 = a[pivot];
+        a[pivot] = a[left];
+        a[left] = temp2;
+        return left;
+    }
+    static void quickSort2(int [] a,int begin,int end){
+        if(end <= begin) return;
+        int pP = partition2(a, begin, end);
+        quickSort(a, begin, pP-1);
+        quickSort(a,pP+1, end);
+    }
+
+    public static void main(String[] args) {
+        int []a={3,4,5,1,-6,10,9,12,2,40,0};
+        quickSort2(a,0,a.length-1);
+        System.out.println(Arrays.toString(a));
     }
 
 }
